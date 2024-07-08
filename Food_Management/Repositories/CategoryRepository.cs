9c412fc01ba09b6bc7ceeed08ef102dd8d24f6b4
Enis ÖZGÜR <zgr.enis@gmail.com>
2024-07-08 13:00:26 +0300

    Category and Food Repositories

---
+++ Food_Management/Repositories/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using Food_Management.Data.Models;
+
+namespace Food_Management.Repositories
+{
+	
+	public class CategoryRepository
+	{
+		Context c = new Context();
+		public List<Category> CategoryList()
+		{
+			return c.Categories.ToList();
+		}
+		public void CategoryAdd(Category ct)
+		{
+			c.Categories.Add(ct);
+			c.SaveChanges();
+		}
+		public void CategoryDelete(Category ct) { 
+			c.Categories.Remove(ct);
+			c.SaveChanges();
+		}
+		public void CategoryUpdate(Category ct)
+		{
+			c.Categories.Update(ct);
+			c.SaveChanges();
+		}
+		public void GetCategory(int id)
+		{
+			c.Categories.Find(id);
+		}
+
+
+	}
+}
---
+++ Food_Management/Repositories/FoodRepository.cs
@@ -0,0 +1,34 @@
+﻿using Food_Management.Data.Models;
+
+namespace Food_Management.Repositories
+{
+	public class FoodRepository
+	{
+		Context c = new Context();
+
+		public void FoodAdd(Food f)
+		{
+			c.Foods.Add(f);
+			c.SaveChanges();
+		}
+		public void FoodRemove(Food f)
+		{
+			c.Foods.Add(f);
+			c.SaveChanges();
+		}
+		public void FoodUpdate(Food f)
+		{
+			c.Foods.Update(f);
+			c.SaveChanges();
+		}
+
+		public List<Food> FoodList()
+		{
+			return c.Foods.ToList();
+		}
+		public void GetFood(int id)
+		{
+			c.Foods.Find(id);
+		}
+	}
+}
