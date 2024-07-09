1718ec94ad3a2b1539cbaddc64e43c7009378c3e
Enis ÖZGÜR <zgr.enis@gmail.com>
2024-07-09 07:45:50 +0300

    genericrepository

--- Food_Management/Repositories/CategoryRepository.cs
+++ Food_Management/Repositories/CategoryRepository.cs
@@ -3,31 +3,9 @@
 namespace Food_Management.Repositories
 {
 	
-	public class CategoryRepository
+	public class CategoryRepository : GenericRepository<Category>
 	{
-		Context c = new Context();
-		public List<Category> CategoryList()
-		{
-			return c.Categories.ToList();
-		}
-		public void CategoryAdd(Category ct)
-		{
-			c.Categories.Add(ct);
-			c.SaveChanges();
-		}
-		public void CategoryDelete(Category ct) { 
-			c.Categories.Remove(ct);
-			c.SaveChanges();
-		}
-		public void CategoryUpdate(Category ct)
-		{
-			c.Categories.Update(ct);
-			c.SaveChanges();
-		}
-		public void GetCategory(int id)
-		{
-			c.Categories.Find(id);
-		}
+		
 
 
 	}
---
+++ Food_Management/Repositories/GenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using Food_Management.Data.Models;
+
+namespace Food_Management.Repositories
+{
+	public class GenericRepository<T> where T: class, new()
+	{
+		Context c = new Context();
+		public List<T> CategoryList()
+		{
+			return c.Set<T>().ToList();
+		}
+		public void CategoryAdd(T t)
+		{
+			c.Set<T>().Add(t);
+			c.SaveChanges();
+		}
+		public void CategoryDelete(T t)
+		{
+			c.Set<T>().Remove(t);
+			c.SaveChanges();
+		}
+		public void CategoryUpdate(T t)
+		{
+			c.Set<T>().Update(t);
+			c.SaveChanges();
+		}
+		public void GetCategory(int id)
+		{
+			c.Set<T>().Find(id);
+		}
+	}
+}
