71e0a9b7ae76a3b44c16854aed78a65fadb2b1b8
Enis ÖZGÜR <zgr.enis@gmail.com>
2024-07-14 16:54:58 +0300

    added adding new category page

--- Food_Management/Controllers/CategoryController.cs
+++ Food_Management/Controllers/CategoryController.cs
@@ -1,16 +1,27 @@
 ﻿using Food_Management.Data.Models;
 using Food_Management.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Food_Management.Data.Models;
 
 namespace Food_Management.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryRepository categoryRepository = new CategoryRepository();
 
         public IActionResult Index()
         {
-            CategoryRepository categoryRepository = new CategoryRepository();
+           
             return View(categoryRepository.TList());
+        }
+        [HttpGet]
+        public IActionResult CategoryAdd() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CategoryAdd(Category p) {
+            categoryRepository.TAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
