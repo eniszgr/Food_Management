99b054126d2b2b8d786bd1391d4ab3840e24f30a
Enis ÖZGÜR <zgr.enis@gmail.com>
2024-08-07 13:49:53 +0300

    Charts was added

---
+++ Food_Management/Controllers/ChartController.cs
@@ -0,0 +1,72 @@
+﻿using Food_Management.Data;
+using Microsoft.AspNetCore.Mvc;
+using Food_Management.Data.Models;
+
+namespace Food_Management.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult VisualizeProductResult() {
+            return Json(ProList());
+        }
+
+        public List<Class1> ProList() {
+            List<Class1> cs = new List<Class1>();
+            cs.Add(new Class1()
+            {
+                proname="Computer",
+                stock=150
+            });
+            cs.Add(new Class1()
+            {
+                proname = "Lcd",
+                stock = 75
+            });
+            cs.Add(new Class1()
+            {
+                proname = "USB Disk",
+                stock = 220
+            });
+            return cs;
+
+        
+        }
+
+        public IActionResult Index3()
+        {
+            return View();
+        }
+        public IActionResult VisualizeProductResult2()
+        {
+            return Json(FoodList());
+        }
+        public List<Class2> FoodList()
+        {
+            List<Class2> cs2 = new List<Class2>();
+            using (var c = new Context()) { 
+                     cs2=c.Foods.Select(x=> new Class2
+                     {
+                         foodname=x.Name,
+                         stock=x.Stock
+                     }).ToList();
+            }
+            return cs2;
+        }
+
+        public IActionResult Index4()
+        {
+            return View();
+        }
+       
+
+    }
+}
---
+++ Food_Management/Data/Class2.cs
@@ -0,0 +1,8 @@
+﻿namespace Food_Management.Data
+{
+    public class Class2
+    {
+        public string foodname { get; set; }
+        public int stock { get; set;}
+    }
+}
