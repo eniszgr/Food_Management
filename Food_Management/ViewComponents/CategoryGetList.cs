e9231cff1d9b6571e2c345b72eb97705ed3cfb4f
Enis ÖZGÜR <zgr.enis@gmail.com>
2024-08-27 14:38:21 +0300

    editied guest page

---
+++ Food_Management/ViewComponents/CategoryGetList.cs
@@ -0,0 +1,17 @@
+﻿using Food_Management.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Food_Management.ViewComponents
+{
+    public class CategoryGetList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+            var categoryList=categoryRepository.TList();
+            return View(categoryList);  
+        }
+
+
+    }
+}
---
+++ Food_Management/ViewComponents/FoodGetList.cs
@@ -0,0 +1,17 @@
+﻿using Food_Management.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Food_Management.ViewComponents
+{
+    public class FoodGetList:ViewComponent
+    {
+       public IViewComponentResult Invoke()
+        {
+            FoodRepository foodRepository = new FoodRepository();
+            var foodlist = foodRepository.TList();
+            return View(foodlist);
+        }
+
+    }
+}
